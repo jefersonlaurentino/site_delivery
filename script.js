d4352af26ef6410e6587a19d28e9203645c51354
Jeferson Laurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2024-03-26 12:43:46 -0300

    correções de erros

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
 let modalkey = 0
 let arrayCar = []
-
+let itemProdutos = []
+let qtdProdutos = 1
 const selecionar=(item)=>document.querySelector(item);
 
 const pizzas = selecionar("#pizzas")
 const pizzas_doces = selecionar("#pizzas_doces")
 const petiscos = selecionar("#petiscos")
+const sucos = selecionar("#sucos")
 const box_card = selecionar("#box_card")
 
 const apiProdutos = "lista_produtos.json"
 fetch(apiProdutos)
 .then(res => res.json())
 .then(res =>{
-    res.forEach(el => {
-        cardProduto.getprodutos(el)
+    itemProdutos = res
+    res.forEach((el,index) => {
+        const produto = selecionar(".card").cloneNode(true);
+        
+        preencheDadosProdutos(produto, el, index)
+
+        produto.addEventListener('click',(e)=>{
+            let chave = key(e)
+
+            abriJanela()
+
+            preencheDadosmodal(el)
+
+            preencherTamanhos(chave)
+
+            selecionar('.qtd-produto').innerHTML = qtdProdutos
+            
+            escolherTamanhoPreco(chave)
+        })
+
+        btnFechar()
     });
 })
 
-class cardProduto {
-    static id = null
-    static type = null
-    static img = null
-    static titulo = null
-    static descricao = null
-    static preco = null
-    
-    static getprodutos=(el)=>{
-        this.id = el.id
-        this.type = el.type
-        this.img = el.img
-        this.titulo = el.titulo
-        this.descricao = el.descricao
-        this.preco = el.preco
-        this.conf = el
-        
-        const produto = selecionar(".card").cloneNode(true);
-        produto.classList.remove("hidden")
-        produto.querySelector(".img").src = this.img
-        produto.querySelector(".img").alt = this.titulo
-        produto.querySelector(".titulo_info").innerText = this.titulo
-        produto.querySelector(".desc_info").innerText = this.descricao
-        produto.querySelector(".preco_info").innerText = formatoReal(this.preco[0])
-        click(produto , this.conf)
-        if (this.conf.type == "pizza") {
-            pizzas.appendChild(produto)
-        } else if (this.conf.type == "pizza_doce") {
-            pizzas_doces.appendChild(produto)
-        } else {
-            petiscos.appendChild(produto)
-        }
+const preencheDadosProdutos= (produto, conf, index) =>{
+    produto.classList.remove("hidden")
+    produto.setAttribute("data-key", index)
+    produto.querySelector(".img").src = conf.img
+    produto.querySelector(".img").alt = conf.titulo
+    produto.querySelector(".titulo_info").innerText = conf.titulo
+    produto.querySelector(".desc_info").innerText = conf.descricao
+    produto.querySelector(".preco_info").innerText = formatoReal(conf.preco[0])
+    if (conf.type == "pizza") {
+        pizzas.appendChild(produto)
+    } else if (conf.type == "pizza_doce") {
+        pizzas_doces.appendChild(produto)
+    } else if (conf.type == "sucos") {
+        sucos.appendChild(produto)
+    } else {
+        petiscos.appendChild(produto)
     }
 }
 
@@ -56,23 +61,9 @@ const formatoReal = (valor) =>{
     return valor.toLocaleString('pt-br',{ style: 'currency',currency: 'BRL'})
 }
 
-// janela card produto
-const click = (elem , conf)=>{
-    let teste = elem.querySelectorAll(".click")
-    const click_produto = [...teste]
-        click_produto.map((el)=>{
-        el.addEventListener('click',(e)=>{
-            abriJanela(conf)
-        })
-    })
-}
-
-// fim janela card
-
-
 // abre e fecha janela 
-const janela = (conf)=>{
-    let tes = box_card.firstElementChild.setAttribute('id',conf.id) 
+const preencheDadosmodal= (conf)=>{
+    box_card.firstElementChild.setAttribute('id',conf.id) 
     selecionar("#img").src = conf.img
     selecionar("#img").alt = conf.titulo
     box_card.querySelector(".titulo_info").innerText = conf.titulo
@@ -82,20 +73,41 @@ const janela = (conf)=>{
     } else {
         box_card.querySelector(".tm").classList.remove("hidden")
     }
-    selecionar("#preco").innerText = formatoReal(conf.preco[modalkey])
-    tamanho_pedido(box_card.querySelectorAll(".tm_p"),conf)
+    selecionar("#preco").innerText = formatoReal(conf.preco[0])
     
-    box_card.querySelector('#voltar').addEventListener('click',()=>{
-        fechaJanela()
-    })
-    box_card.querySelector('#carrinho').addEventListener('click',()=>{
-        fechaJanela()
+}
+
+const key = (e) =>{
+    let key = e.target.parentNode.getAttribute('data-key')
+    
+    qtdProdutos = 1
+
+    modalkey = key
+
+    return key
+}
+
+const preencherTamanhos=() =>{
+    
+    document.querySelectorAll('.tm_p').forEach((size,sizeindex)=>{
+        size.classList.remove('bg-red-600','text-white');
+        (sizeindex == 0) ? size.classList.add('bg-red-600','text-white') : ''
+    });
+}
+
+const escolherTamanhoPreco=(chave)=>{
+    document.querySelectorAll('.tm_p').forEach((size , sizeindex)=>{
+        size.addEventListener('click',()=>{
+            selecionar('.tm_p.bg-red-600').classList.remove('bg-red-600','text-white')
+            size.classList.add('bg-red-600','text-white')
+
+            selecionar('#preco').innerHTML = formatoReal(itemProdutos[chave].preco[sizeindex])
+        })
     })
 }
 
-const abriJanela = (conf) =>{
+const abriJanela = () =>{
     box_card.classList.remove("hidden")
-    janela(conf)
 }
 
 const fechaJanela = () =>{
@@ -103,18 +115,8 @@ const fechaJanela = () =>{
     modalkey = 0
 }
 
-const tamanho_pedido = (tm,conf)=>{
-    tm.forEach((e,index)=>{
-        remove_tamanho(tm)
-        tm[0].classList.add('bg-red-600','text-white')
-        e.addEventListener("click",()=>{
-            remove_tamanho(tm)
-            e.classList.add('bg-red-600','text-white')
-            selecionar("#preco").innerText = formatoReal(conf.preco[index])
-            modalkey = index
-        })
-    })
-// e.classList.add('bg-red-600','text-white')
+const btnFechar=()=>{
+    document.querySelectorAll('#box_card button').forEach((item)=>item.addEventListener('click', fechaJanela))
 }
 
 const remove_tamanho = (tm)=>{
